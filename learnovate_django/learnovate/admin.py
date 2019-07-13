0e46bc1adc415134f33c57b64c6245d3adbbc1ac
Rohith <rohithsingirikonda@gmail.com>
2019-07-13 05:45:28 -0700

    Add Models

--- learnovate_django/learnovate/admin.py
+++ learnovate_django/learnovate/admin.py
@@ -1,3 +1,21 @@
 from django.contrib import admin
+from .models import Category, Topic, ResourceType, Resource
+
 
 # Register your models here.
+class TopicInline(admin.StackedInline):
+    model = Topic
+
+
+class CategoryAdmin(admin.ModelAdmin):
+    inlines = [TopicInline]
+
+
+admin.site.register(Category, CategoryAdmin)
+admin.site.register(Resource)
+admin.site.register(ResourceType)
+admin.site.register(Topic)
+
+admin.site.site_header = "Learnovate Admin"
+admin.site.site_title = "Learnovate Admin"
+admin.site.index_title = "Welcome to Learnovate Admin Portal"
--- learnovate_django/learnovate/models.py
+++ learnovate_django/learnovate/models.py
@@ -1,3 +1,68 @@
+from django.contrib.auth.models import AbstractUser
 from django.db import models
 
-# Create your models here.
+class User(AbstractUser):
+    is_progman = models.BooleanField(default=False)
+    is_teacher = models.BooleanField(default=False)
+
+
+class Teacher(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+
+
+class Category(models.Model):
+    name = models.CharField(max_length=40)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        verbose_name_plural = "Categories"
+
+    def __str__(self):
+        return self.name
+
+
+class Topic(models.Model):
+    name = models.CharField(max_length=40)
+    category = models.ForeignKey(Category, on_delete=models.CASCADE, related_name='topics')
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.name
+
+
+class ResourceType(models.Model):
+    name = models.CharField(max_length=40)
+
+    def __str__(self):
+        return self.name
+
+
+class Resource(models.Model):
+    name = models.CharField(max_length=255)
+    file = models.FileField(null=True, blank=True)
+    description = models.TextField()
+    topic = models.ForeignKey(Topic, on_delete=models.CASCADE, related_name="resources")
+    created_at = models.DateTimeField(auto_now_add=True)
+    type = models.ForeignKey(ResourceType, on_delete=models.CASCADE, related_name='resources')
+
+    def __str__(self):
+        return self.name
+
+
+#
+# class Question(models.Model):
+#     quiz = models.ForeignKey(Quiz, on_delete=models.CASCADE, related_name='questions')
+#     text = models.TextField('Question')
+#     created_at = models.DateTimeField(auto_now_add=True)
+#
+#     def __str__(self):
+#         return self.text
+#
+#
+# class Answer(models.Model):
+#     question = models.ForeignKey(Question, on_delete=models.CASCADE, related_name='answers')
+#     text = models.CharField('Answer', max_length=255)
+#     is_correct = models.BooleanField('Correct answer', default=False)
+#
+#     def __str__(self):
+#         return self.text
